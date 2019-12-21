1605da79b8f7e01f6a6543348190d54a48f3ae11
Emirhan Saraç <emirhan.sarac29@gmail.com>
2019-12-21 20:42:38 +0300

    deneme special

---
+++ concolic/deneme.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.26;
+contract greeter {
+
+
+    /* main function */
+    function greet(bool a, uint72 b, uint256 f, int72 e, int256 ss) returns (uint256) {
+
+        return 1;
+    }
+
+}
+
